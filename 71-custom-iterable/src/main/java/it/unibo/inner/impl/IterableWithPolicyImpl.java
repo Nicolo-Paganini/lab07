b17b65788748204d2074811cf3e1bc4e5a5251fb
Nicolò Paganini <paganininicolo1@gmail.com>
2025-11-07 13:13:20 +0100

    --no-edit

--- 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableWithPolicyImpl.java
+++ 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableWithPolicyImpl.java
@@ -2,7 +2,6 @@ package it.unibo.inner.impl;
 
 import java.util.List;
 import java.util.Iterator;
-import java.util.List;
 import java.util.NoSuchElementException;
 
 import it.unibo.inner.api.IterableWithPolicy;
